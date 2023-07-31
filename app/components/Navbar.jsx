ed1687f74390e68f18b4802eab96517a1df0fde1
William <william.deazevedopro@gmail.com>
2023-07-31 22:33:43 +0200

    Debut navbar container

---
+++ app/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Container from "../Container";
+
+const Navbar = () => {
+  return (
+    <Container>
+      <header>Hello world</header>
+    </Container>
+  );
+};
+
+export default Navbar;
